5f95d32d888cf41d617f1e3f2cdf94fab21f3d2e
tioprayudha <tioprayudha07@gmail.com>
2025-03-17 08:29:33 +0700

    03-reviews

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -5,6 +5,8 @@ const btns = document.querySelectorAll(".btn");
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const styles = e.currentTarget.classList;
+
+    console.log(styles);
     if (styles.contains("decrease")) {
       count--;
     } else if (styles.contains("increase")) {
